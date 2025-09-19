30f5d34f5798244c3cf3c6a61204ad1b346e6158
info2512 <info2512@35186747-lab203.utramigbh.sede>
2025-09-19 17:52:41 -0300

    ProjetoTest

---
+++ src/javaapplicationgittex/JavaApplicationGitTex.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplicationgittex;
+
+/**
+ *
+ * @author info2512
+ */
+public class JavaApplicationGitTex {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
